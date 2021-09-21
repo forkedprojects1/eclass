2cb76e6ba43ff35a3513020e10e64a20ca8d9e5b
Ahmed Ch <ahmedchouihi@gmail.com>
2021-09-21 09:24:23 +0100

    'addTunisianCurrencyIcon'

--- eclass/resources/views/admin/currency/edit.blade.php
+++ eclass/resources/views/admin/currency/edit.blade.php
@@ -51,7 +51,7 @@
    $('.icp-auto').iconpicker({
 
      icons: ['fa fa-inr', 'fa fa-bitcoin', 'fa fa-btc', 'fa fa-cny','fa fa-dollar', 'fa fa-eur', 'fa fa-ngn', 'fa fa-cedi', 'fa fa-rial', 'fa fa-dinar', 'fa fa-tugrik', 'fa fa-brazilian-real', 'fa fa-idr', 'fa fa-shillings', 'fa-gg-circle','fa fa-gg','fa fa-rub','fa fa-ils','fa fa-try','fa fa-krw','fa fa-gbp','fa fa-zar','fa fa-rs','fa fa-pula','fa fa-aud','fa fa-egy','fa fa-taka','fa fa-mal','fa fa-rub','fa fa-brl','fa fa-zwl','fa fa-ngr','fa fa-eutho','fa fa-sgd', 
-     	 'fa fa-lkr', 'fa fa-mad', 'fa fa-thai', 'fa fa-jod', 'fa fa-tsh', 'fa fa-da', 'fa fa-dzd', 'fa fa-rwf'],
+     	 'fa fa-lkr', 'fa fa-mad', 'fa fa-thai', 'fa fa-jod', 'fa fa-tsh', 'fa fa-da', 'fa fa-dzd', 'fa fa-rwf','fa fa-tnd'],
 
     
      selectedCustomClass: 'label label-success',
