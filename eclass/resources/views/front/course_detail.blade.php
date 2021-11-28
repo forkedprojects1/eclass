2a14d85f59b008d3c8921e19fbec7aa0366e0e88
Ahmed Ch <ahmedchouihi@gmail.com>
2021-11-28 22:31:23 +0100

    removeDisqusCommentingSystem

--- eclass/resources/views/front/course_detail.blade.php
+++ eclass/resources/views/front/course_detail.blade.php
@@ -2022,9 +2022,6 @@
 </section>
 <!-- course detail end -->
     <div class="container">
-    <div id="disqus_thread"></div>
-
-    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
     </div>
 
@@ -2035,23 +2032,6 @@
 
 @section('custom-script')
 
-    <script>
-        /**
-         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-
-        var disqus_config = function () {
-        this.page.url = '{{ Request::url() }}';  // Replace PAGE_URL with your page's canonical URL variable
-        this.page.identifier = {{ $course->id }}; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-        };
-
-        (function() { // DON'T EDIT BELOW THIS LINE
-            var d = document, s = d.createElement('script');
-            s.src = 'https://https-www-coursytech-com.disqus.com/embed.js';
-            s.setAttribute('data-timestamp', +new Date());
-            (d.head || d.body).appendChild(s);
-        })();
-    </script>
 <script>
 // Hide the extra content initially, using JS so that if JS is disabled, no problemo:
     $('.read-more-content').addClass('hide_content')
